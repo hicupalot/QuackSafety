26ff1cafb2785a7f6debf1ff4c22eeccdb2d77d0
hicupalot <hicupalot@gmail.com>
2021-03-27 10:05:08 +0000

    Did a lil mistake and made it look messy D: Gave it a nice glow up though :D

--- src/main/java/com/hicupalot/quacksaftey/discord.java
+++ src/main/java/com/hicupalot/quacksaftey/discord.java
@@ -8,7 +8,7 @@ import org.bukkit.command.CommandSender;
 public class discord implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender.hasPermission("quacksaftey.discord")) {
-            sender.sendMessage(ChatColor.GOLD + "[Quack] Join the Discord here" + ChatColor.AQUA + "https://discord.gg/Kx7cVwWESG! " +
+            sender.sendMessage(ChatColor.GOLD + "[Quack] Join the Discord here" + ChatColor.AQUA + " https://discord.gg/Kx7cVwWESG! " +
                     ChatColor.GOLD + "Please remember you have to be 13+ to use Discord!");
         } else {
             sender.sendMessage(ChatColor.GOLD + "[QUACK]" + ChatColor.RED + "You do not have permission to run that command");
