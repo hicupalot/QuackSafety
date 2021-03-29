b1f77b05ad460a026baf0f46510d82df37545d2e
hicupalot <hicupalot@gmail.com>
2021-03-29 13:59:10 +0100

    fixed book ban with OR statements I forgot those whoopsies

--- src/main/java/com/hicupalot/quacksaftey/bookban.java
+++ src/main/java/com/hicupalot/quacksaftey/bookban.java
@@ -1,24 +1,22 @@
 package com.hicupalot.quacksaftey;
+
 import org.bukkit.Material;
-import org.bukkit.entity.*;
-import org.bukkit.event.*;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
-import org.bukkit.event.entity.EntitySpawnEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.EquipmentSlot;
-import org.bukkit.material.*;
-import org.bukkit.entity.Player;
-import org.bukkit.event.Listener;
-import org.bukkit.inventory.*;
+
 public class bookban implements Listener {
     @EventHandler
     public void BookBan(PlayerInteractEvent e) {
         if (e.getHand() == EquipmentSlot.HAND) {
-        if (e.getAction() == Action.RIGHT_CLICK_BLOCK || e.getAction() == Action.RIGHT_CLICK_AIR) {
-        if (e.getClickedBlock().getType() == Material.WRITABLE_BOOK || e.getClickedBlock().getType() == Material.WRITTEN_BOOK) {
-        e.setCancelled(true);
+            if (e.getAction() == Action.RIGHT_CLICK_BLOCK || e.getAction() == Action.RIGHT_CLICK_AIR) {
+                if (e.getClickedBlock().getType() == Material.WRITABLE_BOOK || e.getClickedBlock().getType() == Material.WRITTEN_BOOK) {
+                    e.setCancelled(true);
                 }
             }
         }
 
-    }}
+    }
+}
