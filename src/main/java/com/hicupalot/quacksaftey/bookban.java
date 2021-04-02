e863dd64135d8d219777cc6061ebb54671d6a096
hicupalot <hicupalot@gmail.com>
2021-04-02 08:35:43 +0100

    Cleaned up links

--- src/main/java/com/hicupalot/quacksaftey/bookban.java
+++ src/main/java/com/hicupalot/quacksaftey/bookban.java
@@ -1,15 +1,11 @@
 package com.hicupalot.quacksaftey;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
-import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.EquipmentSlot;
-import org.bukkit.inventory.*;
-import org.bukkit.permissions.*;
 
 public class bookban implements Listener {
     @EventHandler
