709ae65e0aff6ceff74c0669c8f8ad3cbd430a5d
hicupalot <hicupalot@gmail.com>
2021-04-08 10:08:35 +0100

    Fixed Speed LImit

--- src/main/java/com/hicupalot/quacksaftey/SpeedLimit.java
+++ src/main/java/com/hicupalot/quacksaftey/SpeedLimit.java
@@ -1,19 +1,19 @@
 package com.hicupalot.quacksaftey;
-import org.bukkit.Server;
-import org.bukkit.event.inventory.InventoryCreativeEvent;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
-import org.bukkit.event.player.PlayerVelocityEvent;
-import org.bukkit.util.*;
-import org.bukkit.event.*;
-import org.bukkit.*;
+import org.bukkit.ChatColor;
 public class SpeedLimit implements Listener {
     @EventHandler
     public void speedingticket(PlayerMoveEvent e) {
-        if(e.getFrom().distance(e.getTo()) > 20.0)
+        if (e.getFrom().distance(e.getTo()) > 20.0) {
             e.setCancelled(true);
-        Bukkit.broadcast("The user"+ e.getPlayer().getName()+" is possibly hacking/speeding with a minimum " +
+        Bukkit.broadcast(ChatColor.AQUA+"["+ ChatColor.RED+"STAFF"+ ChatColor.AQUA+"]"+ ChatColor.LIGHT_PURPLE + e.getPlayer().getName()+" is possibly hacking/speeding with a minimum " +
                 "velocity of"+
         e.getPlayer().getVelocity()+" Please check on this user", "quacksaftey.speedingticket");
 
     }
-}
+}}
