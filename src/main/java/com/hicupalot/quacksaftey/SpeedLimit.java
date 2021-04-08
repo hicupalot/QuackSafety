f4694b2ecfb8fbc58de1a0e56a298aef801ae3a1
hicupalot <hicupalot@gmail.com>
2021-04-08 12:41:36 +0100

    Cleaned up text

--- src/main/java/com/hicupalot/quacksaftey/SpeedLimit.java
+++ src/main/java/com/hicupalot/quacksaftey/SpeedLimit.java
@@ -12,8 +12,7 @@ public class SpeedLimit implements Listener {
         if (e.getFrom().distance(e.getTo()) > 20.0) {
             e.setCancelled(true);
         Bukkit.broadcast(ChatColor.AQUA+"["+ ChatColor.RED+"STAFF"+ ChatColor.AQUA+"]"+ ChatColor.LIGHT_PURPLE + e.getPlayer().getName()+" is possibly hacking/speeding with a minimum " +
-                "velocity of"+
-        e.getPlayer().getVelocity()+" Please check on this user", "quacksaftey.speedingticket");
+                "velocity of"+ e.getPlayer().getVelocity()+" Please check on this user", "quacksaftey.speedingticket");
 
     }
 }}
