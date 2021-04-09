685e86ea4b9f3d1273871bee3b52774ac98007aa
hicupalot <hicupalot@gmail.com>
2021-04-09 15:25:37 +0100

    Book Ban should now block writable books

--- src/main/java/com/hicupalot/quacksaftey/bookban.java
+++ src/main/java/com/hicupalot/quacksaftey/bookban.java
@@ -7,24 +7,33 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.*;
+import org.bukkit.permissions.*;
+
 public class bookban implements Listener {
     @EventHandler
-
-    public void BookBan(PlayerInteractEvent event) {
-
-        ItemStack itemStack = event.getPlayer().getInventory().getItemInMainHand();
-        ItemStack offHand = event.getPlayer().getInventory().getItemInOffHand();
-        if (event.getAction() != Action.RIGHT_CLICK_AIR && event.getAction() != Action.RIGHT_CLICK_BLOCK) { return; }
-        if ((itemStack.getType() == Material.WRITABLE_BOOK || itemStack.getType() == Material.WRITTEN_BOOK) && !event.getPlayer().hasPermission("quacksaftey.book")) {
-
-            event.getPlayer().closeInventory();
-            event.setCancelled(true);
-
+    public void BookBan(PlayerInteractEvent e) {
+        if (e.getHand() == EquipmentSlot.HAND || e.getHand() == EquipmentSlot.OFF_HAND) {
+            if (e.getAction() == Action.RIGHT_CLICK_BLOCK || e.getAction() == Action.RIGHT_CLICK_AIR) {
+                if (e.getItem()==null) {
+                    return;
+                }
+                // Bukkit.broadcastMessage(e.getItem().getType().name());
+                if (e.getItem().getType() == Material.WRITABLE_BOOK || e.getItem().getType() == Material.WRITTEN_BOOK) {
+                    e.getPlayer().closeInventory();
+                    e.setCancelled(true);
+                }
+            }
         }
 
-        if ((offHand.getType() == Material.WRITABLE_BOOK || offHand.getType() == Material.WRITTEN_BOOK) && !event.getPlayer().hasPermission("quacksaftey.book")) {
 
-            event.getPlayer().closeInventory();
-            event.setCancelled(true);
-
-        }}}
+    }}
