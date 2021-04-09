1bbd0827719645c955acfdd391c2394683de97f2
hicupalot <hicupalot@gmail.com>
2021-04-09 13:14:58 +0100

    Book Ban should now block writable books

--- src/main/java/com/hicupalot/quacksaftey/bookban.java
+++ src/main/java/com/hicupalot/quacksaftey/bookban.java
@@ -5,23 +5,26 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.ItemStack;
 
 public class bookban implements Listener {
     @EventHandler
-    public void BookBan(PlayerInteractEvent e) {
-        if (e.getHand() == EquipmentSlot.HAND || e.getHand() == EquipmentSlot.OFF_HAND) {
-            if (e.getAction() == Action.RIGHT_CLICK_BLOCK || e.getAction() == Action.RIGHT_CLICK_AIR) {
-                if (e.getItem()==null) {
-                    return;
-                }
-               // Debug Mode below [Shows Item Name]
-               // Bukkit.broadcastMessage(e.getItem().getType().name());
-                if (e.getItem().getType() == Material.WRITABLE_BOOK || e.getItem().getType() == Material.WRITTEN_BOOK) {
-                e.setCancelled(true);
-                }
-            }
+
+    public void BookBan(PlayerInteractEvent event) {
+
+        ItemStack itemStack = event.getPlayer().getInventory().getItemInMainHand();
+        ItemStack offHand = event.getPlayer().getInventory().getItemInOffHand();
+        if (event.getAction() != Action.RIGHT_CLICK_AIR && event.getAction() != Action.RIGHT_CLICK_BLOCK) { return; }
+        if ((itemStack.getType() == Material.WRITABLE_BOOK || itemStack.getType() == Material.WRITTEN_BOOK) && !event.getPlayer().hasPermission("quacksaftey.book")) {
+
+            event.getPlayer().closeInventory();
+            event.setCancelled(true);
+
         }
 
+        if ((offHand.getType() == Material.WRITABLE_BOOK || offHand.getType() == Material.WRITTEN_BOOK) && !event.getPlayer().hasPermission("quacksaftey.book")) {
 
-    }}
+            event.getPlayer().closeInventory();
+            event.setCancelled(true);
+
+        }}}
