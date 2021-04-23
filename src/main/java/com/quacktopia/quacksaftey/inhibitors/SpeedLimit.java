83f29db53b32e4104c044266f4c7bc6d2e74b39d
hicupalot <hicupalot@gmail.com>
2021-04-23 07:45:44 +0100

    Added multiple timers so each event is mutually exclusive

--- src/main/java/com/quacktopia/quacksaftey/inhibitors/SpeedLimit.java
+++ src/main/java/com/quacktopia/quacksaftey/inhibitors/SpeedLimit.java
@@ -4,37 +4,69 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.block.BlockFace;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerVelocityEvent;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.HashMap;
+import java.util.UUID;
+
 public class SpeedLimit implements Listener {
     @SuppressWarnings("deprecation")
+    private HashMap<UUID, Long> cooldownspeed = new HashMap<UUID,Long>();
+    private int cooldowntime = 3600;
+    private int emergencyspeed = 10;
+    private HashMap<UUID, Long> flycooldown = new HashMap<UUID,Long>();
+    private HashMap<UUID, Long> supercooldown = new HashMap<UUID,Long>();
     @EventHandler(priority = EventPriority.LOW)
     public void speedingticket(PlayerVelocityEvent e) {
+        Player p = e.getPlayer();
         if (!e.getPlayer().isFlying()) {
             if (e.getPlayer().getFallDistance() > 5) {
                 return;
             }
             if (e.getVelocity().lengthSquared() > 4 && (e.getVelocity().lengthSquared() < 10)) {
+                Bukkit.broadcast(ChatColor.AQUA + "[" + ChatColor.RED + "STAFF" + ChatColor.AQUA + "]" + ChatColor.LIGHT_PURPLE + e.getPlayer().getName() + " is possibly hacking/speeding with a minimum " +
+                        "velocity of" + e.getPlayer().getVelocity() + " Please check on this user", "quacksafety.speedingticket");
+                cooldownspeed.put(p.getUniqueId(), System.currentTimeMillis());
+
+            }
+            if (cooldownspeed.containsKey(p.getUniqueId())) {
+                long secondsleft = (System.currentTimeMillis() - cooldownspeed.get(p.getUniqueId()));
+                if (secondsleft <= 0) {
                     Bukkit.broadcast(ChatColor.AQUA + "[" + ChatColor.RED + "STAFF" + ChatColor.AQUA + "]" + ChatColor.LIGHT_PURPLE + e.getPlayer().getName() + " is possibly hacking/speeding with a minimum " +
                             "velocity of" + e.getPlayer().getVelocity() + " Please check on this user", "quacksafety.speedingticket");
+                    cooldownspeed.put(p.getUniqueId(), System.currentTimeMillis());
+                } else {
+                    return;
+
                 }
             }
         }
+    }
     @EventHandler
     @SuppressWarnings("deprecation")
     public void flyingspeedticket(PlayerVelocityEvent e) {
+        Player p = e.getPlayer();
         if (e.getPlayer().isFlying()) {
             if (e.getVelocity().lengthSquared() > 6 && e.getVelocity().lengthSquared() < 15) {
                 Bukkit.broadcast(ChatColor.AQUA + "[" + ChatColor.RED + "STAFF" + ChatColor.AQUA + "]" + ChatColor.LIGHT_PURPLE + e.getPlayer().getName() + " is possibly hacking/speeding with a minimum " +
                         "velocity of" + e.getPlayer().getVelocity() + " Please check on this user", "quacksafety.speedingticket");
+                flycooldown.put(p.getUniqueId(), System.currentTimeMillis());
+            }
+            if (flycooldown.containsKey(p.getUniqueId())) {
+                long secondsleft = (System.currentTimeMillis() - flycooldown.get(p.getUniqueId()));
+                if (secondsleft <= 0) {
+                    Bukkit.broadcast(ChatColor.AQUA + "[" + ChatColor.RED + "STAFF" + ChatColor.AQUA + "]" + ChatColor.LIGHT_PURPLE + e.getPlayer().getName() + " is possibly hacking/speeding with a minimum " +
+                            "velocity of" + e.getPlayer().getVelocity() + " Please check on this user", "quacksafety.speedingticket");
+                    flycooldown.put(p.getUniqueId(), System.currentTimeMillis());
+                }
             }
         }
     }
-
     @EventHandler(priority = EventPriority.NORMAL)
     @SuppressWarnings("deprecation")
     public void majorspeedingviolation(PlayerVelocityEvent e) {
