c77b733f69075328aefc8b653fa39b40f222fd76
hicupalot <hicupalot@gmail.com>
2021-05-02 08:09:09 +0100

    Removed StaffJoin and added it to a Bungee Plugin

--- src/main/java/com/quacktopia/quacksaftey/QuackSaftey.java
+++ src/main/java/com/quacktopia/quacksaftey/QuackSaftey.java
@@ -1,22 +1,10 @@
 package com.quacktopia.quacksaftey;
 
-import com.plotsquared.core.api.PlotAPI;
 import com.quacktopia.quacksaftey.commands.*;
 import com.quacktopia.quacksaftey.inhibitors.*;
 import events.LavaPlace;
-import events.StaffJoin;
-import net.milkbowl.vault.chat.Chat;
-import net.milkbowl.vault.economy.Economy;
-import net.milkbowl.vault.economy.EconomyResponse;
-import net.milkbowl.vault.permission.Permission;
 import org.bukkit.ChatColor;
-import org.bukkit.entity.Player;
-import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.util.ArrayList;
-import java.util.UUID;
-import java.util.concurrent.TimeUnit;
 
 public final class QuackSaftey extends JavaPlugin {
 
@@ -39,7 +27,6 @@ public final class QuackSaftey extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new LaggOmeter(), this);
         getServer().getPluginManager().registerEvents(new SpeedLimit(), this);
         getServer().getPluginManager().registerEvents(new commandprefix(), this);
-        getServer().getPluginManager().registerEvents(new StaffJoin(), this);
         getServer().getPluginManager().registerEvents(new LavaPlace(), this);
         System.out.println(ChatColor.GOLD + "[QuackSafety] Inhibitors and Events loaded");
         System.out.println(ChatColor.GOLD + "[QuackSaftey] QuackSaftey v" + getDescription().getAPIVersion() + " By Authors" + getDescription().getAuthors());
