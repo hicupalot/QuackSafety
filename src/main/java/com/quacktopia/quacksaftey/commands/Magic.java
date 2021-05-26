1a6678caf92d4477e8c4b121ddb4c6e55c47ca47
hicupalot <hicupalot@gmail.com>
2021-05-26 19:37:54 +0100

    Added option to cast magic on only one player

--- src/main/java/com/quacktopia/quacksaftey/commands/Magic.java
+++ src/main/java/com/quacktopia/quacksaftey/commands/Magic.java
@@ -16,12 +16,25 @@ public class Magic implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender instanceof Player || sender instanceof ConsoleCommandSender) {
             if (sender.hasPermission("quacksafety.magic")) {
-                Player online = (Player) Bukkit.getOnlinePlayers();
-                online.playSound(online.getLocation(), Sound.ENTITY_LIGHTNING_BOLT_IMPACT, 10, 10);
-                online.addPotionEffect(new PotionEffect(PotionEffectType.LEVITATION, 5, 2, true, false));
-                Bukkit.broadcast(ChatColor.YELLOW + "[" + ChatColor.RED + "MAGIC" + ChatColor.YELLOW + "]" + ChatColor.LIGHT_PURPLE + sender.getName() + " cast Wingardium Leviosa!", "quacksafety.magicreviever");
+                if (args.length == 0) {
+                    Player online = (Player) Bukkit.getOnlinePlayers();
+                    online.getWorld().strikeLightning(online.getLocation());
+                    online.addPotionEffect(new PotionEffect(PotionEffectType.LEVITATION, 5, 2, true, false));
+                    Bukkit.broadcast(ChatColor.YELLOW + "[" + ChatColor.RED + "MAGIC" + ChatColor.YELLOW + "]" + ChatColor.LIGHT_PURPLE + sender.getName() + " cast Wingardium Leviosa!", "quacksafety.magicreviever");
+                }
             }
+            if (args.length == 1) {
+                Player target = (Player) Bukkit.getServer().getPlayer(args[1]);
+                target.getWorld().strikeLightning(target.getLocation());
+                target.addPotionEffect(new PotionEffect(PotionEffectType.LEVITATION, 5, 2, true, false));
+                target.sendMessage("[" + ChatColor.RED + "MAGIC" + ChatColor.YELLOW + "]" + ChatColor.LIGHT_PURPLE + "You had Wingardium Leviosa cast on you by " + sender.getName());
+            }
+            if (args.length>1){
+                sender.sendMessage(ChatColor.RED+"Please do /magic to cast it on the entire server and /magic player to cast it on the entire server");
+            }
+            return false;
         }
-    return false;
+   return false;
     }
 }
+
